dbd8c7b3b815654983fca8bec7539866441c3eba
scottseo000 <scottseo96@gmail.com>
2024-10-22 14:39:38 -0400

    added price check test to pricing module

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -2,16 +2,40 @@ package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.http.MediaType;
 
-//TODO add new tests with MockMvc
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	//mvn build succeeds, but test fails
+	//TODO check if running everything fixes the test
+	@Test
+	public void getVehiclePrice() throws Exception {
+		int testId = 1;
+		mockMvc.perform(MockMvcRequestBuilders
+				.get("/prices/{vehicleId}", String.valueOf(testId)))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.vehicleId").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.price").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.currency").isString());
+	}
 }
